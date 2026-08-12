0460ab9f6e86c0e3ddc837dd621d32fbaf85a5d7
Aditya70-creator <mitraaditya14@gmail.com>
2026-08-13 00:58:33 +0530

    Mad Libs Game and Shopping Cart Program

---
+++ src/SHOPPING_CART_PROGRAM.java
@@ -0,0 +1,33 @@
+import com.sun.security.jgss.GSSUtil;
+
+import java.util.*;
+public class SHOPPING_CART_PROGRAM {
+    public static void main(String[] args) {
+
+        // SHOPPING CART PROGRAM
+
+        Scanner sc = new Scanner(System.in);
+
+        String item;
+        double price;
+        int quantity;
+        char currency = '$';
+        double total;
+
+        System.out.print("What item would you like to buy?: ");
+        item = sc.nextLine();
+
+        System.out.print("What is the price for each?: ");
+        price=sc.nextDouble();
+
+        System.out.print("How many would you like?: ");
+        quantity=sc.nextInt();
+
+        total=price*quantity;
+
+        System.out.println("\nYou have bought " + quantity + " " + item + "/s");
+        System.out.println("Your total is " + currency + total);
+
+        sc.close();
+    }
+}
